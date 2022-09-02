1cf524c496ba60ec7f7c2725e0343293b1d0fe17
petushka1 <nadya@devery.io>
2022-09-02 21:07:34 +0300

    add working solution for all? method

--- enumerable_spec.rb
+++ enumerable_spec.rb
@@ -1,27 +1,35 @@
-def my_all
-    result = true
-    [1, 2, 3].each do |e|
-    check = yield e
-        result = false unless check
-        break
+# module with Enumerable methods built and implemented
+
+module MyEnumerable
+
+    def all?
+        result = true
+        [1, 2, 3].each do |e|
+        check = yield e
+            result = false unless check
+            break
+        end
+        puts result
     end
-    puts result
+
 end
 
-my_all { |e| e > 1 }
-
+# Class with instance variable impelmented
 
 class MyList
+include MyEnumerable
+
     def initialize (*number)
         @list = number
     end
 
-    def test_class 
-        puts @list
+    def each(&block)
+        yield( @list )
+        @list.all? &block
     end
 end
 
 list = MyList.new(1, 2, 3)
-list.test_class()
+list.all? { |e| e > 1 }
 
 
